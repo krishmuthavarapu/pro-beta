9728fd5a294a7bba5e42bde660f70c19bd5d0c0c
sai <krishmuthavarapu@gmail.com>
2020-09-17 19:47:11 +0530

    asg2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  userName = 'Enter';
+  disableBtn = true;
+  constructor() {}
 
   ngOnInit(): void {
+    if (this.userName) {
+      this.disableBtn = false;
+    }
   }
 
+  onResetUsername() {
+    if ((this.userName = '')) {
+      this.disableBtn = false;
+    } else {
+      this.userName = '';
+      this.disableBtn = true;
+    }
+  }
+  onUsernameUpdate(event: any) {
+    this.disableBtn = false;
+  }
 }
--- src/app/sucess-alert/sucess-alert.component.ts
+++ src/app/sucess-alert/sucess-alert.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sucess-alert',
   templateUrl: './sucess-alert.component.html',
-  styleUrls: ['./sucess-alert.component.css']
+  styleUrls: ['./sucess-alert.component.css'],
 })
 export class SucessAlertComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  allownewserver = true;
+  serverCreationStatus = 'Server was not created';
+  inputValue = 'Enter';
+  constructor() {
+    // setTimeout(() => {
+    //   this.allownewserver = false;
+    // }, 2000);
   }
 
+  ngOnInit(): void {}
+  onCreateServer() {
+    this.serverCreationStatus = 'Server is Created';
+    this.allownewserver = false;
+  }
+  onRemoveServer() {
+    if ((this.serverCreationStatus = 'Server was not created')) {
+      this.allownewserver = true;
+    } else {
+      this.serverCreationStatus = 'Server is Removed';
+    }
+  }
+  onUpdateInput(event: any) {
+    console.log(event);
+    this.inputValue = event.target.value;
+  }
 }
